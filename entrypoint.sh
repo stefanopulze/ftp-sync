dfa346d35137095178628714340e2eebc1f1ceac
Stefano Pulze <stefano.pulze@infocamere.it>
2022-04-20 21:58:22 +0200

    Add log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 lftp "${INPUT_SERVER}" -u "${INPUT_USERNAME},${INPUT_PASSWORD}" \
--e "set ftp:use-feat off; set ftp:ssl-force $INPUT_FORCESSL; set ftp:ssl-allow ${INPUT_ALLOWSSL}; mirror --reverse -n -P=5 ${INPUT_LOCALDIR} ${INPUT_REMOTEDIR}; quit"
+-e "set ftp:use-feat off; set ftp:ssl-force $INPUT_FORCESSL; set ftp:ssl-allow ${INPUT_ALLOWSSL}; mirror --reverse -n -P=5 -v ${INPUT_LOCALDIR} ${INPUT_REMOTEDIR}; quit"
 
 echo "::set-output name=result::$?"
