c31b8e9713d24a2ba35d8e2a4b151197709f0a72
Stefano Pulze <stefano.pulze@infocamere.it>
2022-04-20 22:03:01 +0200

    Overwrite file without remove first

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 lftp "${INPUT_SERVER}" -u "${INPUT_USERNAME},${INPUT_PASSWORD}" \
--e "set ftp:use-feat off; set ftp:ssl-force $INPUT_FORCESSL; set ftp:ssl-allow ${INPUT_ALLOWSSL}; mirror --reverse --continue --dereference -n --parallel=5 -v -x ^\.git/$ ${INPUT_LOCALDIR} ${INPUT_REMOTEDIR}; quit"
+-e "set ftp:use-feat off; set ftp:ssl-force $INPUT_FORCESSL; set ftp:ssl-allow ${INPUT_ALLOWSSL}; mirror --reverse --continue --dereference --overwrite -n --parallel=5 -v -x ^\.git/$ ${INPUT_LOCALDIR} ${INPUT_REMOTEDIR}; quit"
 
 echo "::set-output name=result::$?"
